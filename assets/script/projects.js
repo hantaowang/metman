f704b0c2c9381fab957f52b494f3e7a1fce3ece3
hantaowang <hantaowang97@gmail.com>
2017-01-01 18:54:45 -0800

    Add files via upload

--- assets/script/projects.js
+++ assets/script/projects.js
@@ -3,30 +3,38 @@ $(document).ready(function(){
   $(".item").on('click', function() {
       var name = $(this).attr('id')[1];
       var state = $(this).data('state');
+      state = 1;
       state = !state;
-      var sname = "s" + name
-      var ele = document.getElementById(sname);
       if (state) {
           $(this).css("background-color", "#3ABEFF");
           $(this).css("color", "#f1f1f1");
           $("#d" + name).slideDown();
-          ele.innerHTML = '-';
-          $("#s" + name).css("line-height", "25px");
       } else {
           $(this).css("background-color", "#f1f1f1");
           $(this).css("color", "black");
           $("#d" + name).slideUp();
-          ele.innerHTML = '+';
-          $("#s" + name).css("line-height", "30px");
       }
       $(this).data('state', state);
   });
 
 
+  $("#resume").click(function(){
+    $("#mo1").fadeIn(350);
+  });
+
+  $("#servo").click(function(){
+    $("#mo2").fadeIn(350);
+  });
+
+  $("#servoessay").click(function(){
+    $("#mo3").fadeIn(350);
+  });
 
 
-
-
+  $(".mainoverlay").click(function(){
+    $(".mainoverlay").fadeOut(350);
+    $(".main").enable();
+  });
 
 
 
