762273c9f4a5931e65faf721e70a104094ce5123
hantaowang <willwang@Willys-MacBook-Pro.local>
2017-01-05 22:40:48 -0800

    javascript for index.html

--- assets/script/script.js
+++ assets/script/script.js
@@ -56,6 +56,19 @@ $(document).ready(function(){
     document.canvas.src = imagesArray[num];
   });
 
+  var _gauges = _gauges || [];
+  (function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '586f3b38bb922a43190830ea');
+  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+  t.src = 'https://d36ee2fcip1434.cloudfront.net/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
+  })();
+
   var text = ["Welcome", "Sup.", "Alola", "Greetings", "Hello"];
   var counter = 0;
   var elem = document.getElementById("part5title");
