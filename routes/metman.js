c148b5b7e75f51331d11cdfb5868283086102467
Will Wang <hwang97@berkeley.edu>
2017-04-27 14:13:04 -0700

    fixed metman mode0 inouts

--- routes/metman.js
+++ routes/metman.js
@@ -5,13 +5,13 @@ var fs = require("fs");
 var exec = require('child_process').execFile;
 
 
-var mode0fields =  ["UEFF", "RM", "AC", "ARI", "G", "PCAB",
-    "VCAB", "TCAB", "TDEWC", "CPG", "TW", "ALUG", "ALUG", "AKUG",
-    "EUG", "VF", "LCG", "WF", "DT", "SETI", "PRINTI", "MCASES", "QASRB", "TWI", "HEIGHT", "WEIGHT"];
+var mode0fields =  ["RM", "UEFF", "HEIGHT", "WEIGHT", "G", "PCAB",
+    "TCAB", "TDEWC", "TW", "VCAB", "VF", "AKUG", "ALUG",
+    "EUG", "LCG", "TWI", "WF", "DT", "MCASES", "PRINTI", "SETI"];
 
-var mode0defaults = ["0", "800", "19.5", "15.5", "1", "14.7",
-    "25", "70", "50", "0.24", "70", "0.0423", "0.0141",
-    "0.046", "0.9", "1.0", "F", "0", "0.05", "180", "60", "0", "10*0.0", "62", "67", "154"];
+var mode0defaults = ["800.0", "0.0", "67.0", "154.0", "1", "14.7",
+    "70.0", "50.0", "70.0", "25.0", "1.0", "0.046", "0.0141",
+    "0.90", "F", "62.0", "0.0", "0.05", "0.0", "60.0", "180.0"];
 
 var mode1fields =  ["UEFF", "RM", "AC", "ARI", "G", "PCAB",
     "VCAB", "TCAB", "TDEWC", "CPG", "TW", "ALUG", "ALUG", "AKUG",
@@ -45,17 +45,17 @@ function runFortran(len, params) {
                     return console.log(err);
                 }
             });
-            exec('MetManFortran.exe', function(error, stdout, stderr) {
+            exec('./public/files/MetManFortran.exe', function(error, stdout, stderr) {
                 console.log(error)
                 console.log(stdout.toString());
                 console.log(stderr.toString());
             });
-            // fs.readFile('./public/files/case.out', 'utf8', function (err,data) {
-            //     if (err) {
-            //         return console.log(err);
-            //     }
-            //     return data;
-            // });
+            fs.readFile('./public/files/case.out', 'utf8', function (err,data) {
+                if (err) {
+                    return console.log(err);
+                }
+                return data;
+            });
         } catch (err) {
             return err.stack;
         }
